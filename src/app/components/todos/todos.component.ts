f785e553b73be39ec7cfb84ce4525a5a892a56ce
Kishor Rathva <kishor.rathva@rapidops.com>
2019-03-07 18:53:03 +0530

    app-routing

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,3 @@
-
-
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from './../../services/todo.service';
 import { Todo } from '../../models/Todos';
@@ -23,5 +21,11 @@ export class TodosComponent implements OnInit {
     this.todos = this.todos.filter(t => t.id !== todo.id);
     // Remove from server
     this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    })
   }
 }
